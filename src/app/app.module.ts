ad1fe129a4966169731b74de3cba5ba7a0311cc5
Ayush Goyal <ayush987goyal@gmail.com>
2017-07-16 11:56:32 +0530

    commit till navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SmoothScrollToDirective, SmoothScrollDirective } from "ng2-smooth-scroll";
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SmoothScrollDirective,
+    SmoothScrollToDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
