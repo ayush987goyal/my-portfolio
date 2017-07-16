c039a4305b9fb65abf1c40cdd40e2344d3c4fc7a
Ayush Goyal <ayush987goyal@gmail.com>
2017-07-16 20:21:07 +0530

    fix for img paths

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,14 @@ import { NgForm } from "@angular/forms";
 export class AppComponent {
   selectedSection = 'about';
   @ViewChild('f') mainForm: NgForm;
-  portfolioData = [{ "name": 'Tic Tac Toe', "img": '../assets/tictactoe.PNG', "link": 'https://codepen.io/ayush987goyal/full/WRNaMX/' },
-  { "name": 'Simon Game', "img": '../assets/simongame.PNG', "link": 'https://codepen.io/ayush987goyal/full/egmjEw/' },
-  {"name": 'Markdown Previewer', "img": '../assets/markdown.PNG', "link": 'https://ayush987goyal.github.io/mardown-previewer/'},
-  {"name": "Camper Leaderboard", "img": '../assets/leaderboard.PNG', "link": 'https://ayush987goyal.github.io/camper-leaderboard/'},
-  { "name": 'Pomodoro Clock', "img": '../assets/pomodoroclock.PNG', "link": 'https://codepen.io/ayush987goyal/full/apbmRj/' },
-  { "name": 'Wikipedia Viewer', "img": '../assets/wikiviewer.PNG', "link": 'https://codepen.io/ayush987goyal/full/QdLwzq/' },
-  { "name": 'Random Joke Machine', "img": '../assets/jokemachine.PNG', "link": 'https://codepen.io/ayush987goyal/full/PbMpJz/' },
-  { "name": 'Calculator', "img": '../assets/calculator.PNG', "link": 'https://codepen.io/ayush987goyal/full/dNyGRQ/' }];
+  portfolioData = [{ "name": 'Tic Tac Toe', "img": './assets/tictactoe.PNG', "link": 'https://codepen.io/ayush987goyal/full/WRNaMX/' },
+  { "name": 'Simon Game', "img": './assets/simongame.PNG', "link": 'https://codepen.io/ayush987goyal/full/egmjEw/' },
+  {"name": 'Markdown Previewer', "img": './assets/markdown.PNG', "link": 'https://ayush987goyal.github.io/mardown-previewer/'},
+  {"name": "Camper Leaderboard", "img": './assets/leaderboard.PNG', "link": 'https://ayush987goyal.github.io/camper-leaderboard/'},
+  { "name": 'Pomodoro Clock', "img": './assets/pomodoroclock.PNG', "link": 'https://codepen.io/ayush987goyal/full/apbmRj/' },
+  { "name": 'Wikipedia Viewer', "img": './assets/wikiviewer.PNG', "link": 'https://codepen.io/ayush987goyal/full/QdLwzq/' },
+  { "name": 'Random Joke Machine', "img": './assets/jokemachine.PNG', "link": 'https://codepen.io/ayush987goyal/full/PbMpJz/' },
+  { "name": 'Calculator', "img": './assets/calculator.PNG', "link": 'https://codepen.io/ayush987goyal/full/dNyGRQ/' }];
 
   onSectionChange(section) {
     this.selectedSection = section;
