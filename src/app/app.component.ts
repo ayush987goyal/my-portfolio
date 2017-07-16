a603305966edfcbb183c34239ad57947ed801bde
Ayush Goyal <ayush987goyal@gmail.com>
2017-07-16 20:57:32 +0530

    using git urls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,14 @@ import { NgForm } from "@angular/forms";
 export class AppComponent {
   selectedSection = 'about';
   @ViewChild('f') mainForm: NgForm;
-  portfolioData = [{ "name": 'Tic Tac Toe', "img": 'my-portfolio/assets/tictactoe.PNG', "link": 'https://codepen.io/ayush987goyal/full/WRNaMX/' },
-  { "name": 'Simon Game', "img": 'my-portfolio/assets/simongame.PNG', "link": 'https://codepen.io/ayush987goyal/full/egmjEw/' },
-  {"name": 'Markdown Previewer', "img": 'my-portfolio/assets/markdown.PNG', "link": 'https://ayush987goyal.github.io/mardown-previewer/'},
-  {"name": "Camper Leaderboard", "img": 'my-portfolio/assets/leaderboard.PNG', "link": 'https://ayush987goyal.github.io/camper-leaderboard/'},
-  { "name": 'Pomodoro Clock', "img": 'my-portfolio/assets/pomodoroclock.PNG', "link": 'https://codepen.io/ayush987goyal/full/apbmRj/' },
-  { "name": 'Wikipedia Viewer', "img": 'my-portfolio/assets/wikiviewer.PNG', "link": 'https://codepen.io/ayush987goyal/full/QdLwzq/' },
-  { "name": 'Random Joke Machine', "img": 'my-portfolio/assets/jokemachine.PNG', "link": 'https://codepen.io/ayush987goyal/full/PbMpJz/' },
-  { "name": 'Calculator', "img": 'my-portfolio/assets/calculator.PNG', "link": 'https://codepen.io/ayush987goyal/full/dNyGRQ/' }];
+  portfolioData = [{ "name": 'Tic Tac Toe', "img": 'https://raw.githubusercontent.com/ayush987goyal/my-portfolio/master/src/assets/tictactoe.PNG', "link": 'https://codepen.io/ayush987goyal/full/WRNaMX/' },
+  { "name": 'Simon Game', "img": 'https://raw.githubusercontent.com/ayush987goyal/my-portfolio/master/src/assets/simongame.PNG', "link": 'https://codepen.io/ayush987goyal/full/egmjEw/' },
+  {"name": 'Markdown Previewer', "img": 'https://raw.githubusercontent.com/ayush987goyal/my-portfolio/master/src/assets/markdown.PNG', "link": 'https://ayush987goyal.github.io/mardown-previewer/'},
+  {"name": "Camper Leaderboard", "img": 'https://raw.githubusercontent.com/ayush987goyal/my-portfolio/master/src/assets/leaderboard.PNG', "link": 'https://ayush987goyal.github.io/camper-leaderboard/'},
+  { "name": 'Pomodoro Clock', "img": 'https://raw.githubusercontent.com/ayush987goyal/my-portfolio/master/src/assets/pomodoroclock.PNG', "link": 'https://codepen.io/ayush987goyal/full/apbmRj/' },
+  { "name": 'Wikipedia Viewer', "img": 'https://raw.githubusercontent.com/ayush987goyal/my-portfolio/master/src/assets/wikiviewer.PNG', "link": 'https://codepen.io/ayush987goyal/full/QdLwzq/' },
+  { "name": 'Random Joke Machine', "img": 'https://raw.githubusercontent.com/ayush987goyal/my-portfolio/master/src/assets/jokemachine.PNG', "link": 'https://codepen.io/ayush987goyal/full/PbMpJz/' },
+  { "name": 'Calculator', "img": 'https://raw.githubusercontent.com/ayush987goyal/my-portfolio/master/src/assets/calculator.PNG', "link": 'https://codepen.io/ayush987goyal/full/dNyGRQ/' }];
 
   onSectionChange(section) {
     this.selectedSection = section;
